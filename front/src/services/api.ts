3cbebc879b22d79f2a7ff8e310163d08a393d8be
Anael CHARDAN <anael.chardan@spendesk.com>
2023-08-09 00:55:18 +0200

    ts-rest-client

--- front/src/services/api.ts
+++ front/src/services/api.ts
@@ -1,3 +1,8 @@
-export const getHealth = async () => {
-    return "TODO";
-}
+import { initQueryClient } from '@ts-rest/react-query';
+import { tsRestContract } from '../../../back/src/infrastructure/http/contracts/ts-rest-contract';
+import { config } from '../config';
+
+export const client = initQueryClient(tsRestContract, {
+  baseUrl: config.api_url,
+  baseHeaders: {},
+});
